c193349df2a50bd00aed1cb93bbdbcc3770fd007
Brita Ramsay <britaramsay@gmail.com>
2018-03-15 12:58:02 -0400

    10 static images appear based on what was clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,18 +12,22 @@ $(document).ready(function () {
     $("#topics").on("click", ".btn", function() { 
         var title = $(this).attr("data-name");   
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + title + "&limit=1&api_key=dc6zaTOxFJmzC";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + title + "&limit=10&api_key=dc6zaTOxFJmzC";
     
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
-
-            var gif = $('<div>');
-            gif.append($('<img>',{id:'gif',src:response.data[0].images.fixed_height.url}));
-
-            $('#topics').append(gif);
+            $('#gifs').empty();
+            (response.data).forEach (function (data) {
+                var gif = $('<div>');
+                
+                gif.append($('<img>',{id:'gif',src:data.images.fixed_height_still.url}));
+    
+                $('#gifs').append(gif);
+            });
+            
 
         });
     
