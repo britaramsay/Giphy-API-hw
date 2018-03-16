d432f738a4101800aae4870286282a8e0878d5e0
Brita Ramsay <britaramsay@gmail.com>
2018-03-16 14:20:10 -0400

    Images change if a new category is chosen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,15 @@
 // List of topics for gifs
-var topics = ['cats', 'dogs', 'snakes', 'snails'];
+var topics = ['cats', 'dogs', 'snakes', 'ferret'];
+var $this = '';
+var title;
+
 
 $(document).ready(function () { 
+    renderButtons();
+});
+
+function renderButtons () { 
+    $('#topics').empty();
     // For each topic in the array 
     topics.forEach( function (topic) {  
         // Create a button element
@@ -14,58 +22,68 @@ $(document).ready(function () {
         // Append to topics div
         $('#topics').append(button);
     });
-    // When a topic button is clicked
-    $("#topics").on("click", ".btn", function() { 
-        // Set title to data-name for that button
-        var title = $(this).attr("data-name");   
-        // Define query url with search as the topic
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + title + "&limit=10&api_key=dc6zaTOxFJmzC";
-        // Use ajax to get a response with that url
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            // Empty gifs div from a previous button being clicked
-            $('#gifs').empty();
-            // For each gif in the response
-            (response.data).forEach (function (data) {
-                // Create div for image and rating
-                var imgDiv = $('<div>');
-                imgDiv.addClass('image');
-                // Create img element
-                var gif = $('<img>');
+}
+
+
+function callAPI () {  
+    // Set title to data-name for that button
+    title = $(this).attr("data-name");   
+    console.log($(this).attr("data-name"));
+    // Define query url with search as the topic
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + title + "&limit=10&api_key=dc6zaTOxFJmzC";
+    // Use ajax to get a response with that url
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        // Empty gifs div from a previous button being clicked
+        $('#gifs').empty();
+        // For each gif in the response
+        (response.data).forEach (function (data) {
+            // Create div for image and rating
+            var imgDiv = $('<div>')
+                .addClass('image');
+            // Create img element
+            var gif = $('<img>')
+                .addClass('img')
                 // Set src attribute to the still img url
-                gif.attr('src', data.images.fixed_height_still.url);
+                .attr('src', data.images.fixed_height_still.url)
                 // Set srcStill attribute to the still img url
-                gif.attr('srcStill', data.images.fixed_height_still.url);
+                .attr('srcStill', data.images.fixed_height_still.url)
                 // Set srcMove attribute to the moving gif url
-                gif.attr('srcMove', data.images.fixed_height.url);
-                // Create div for rating
-                var rating = $('<p>');
+                .attr('srcMove', data.images.fixed_height.url);
+                
+            // Create div for rating
+            var rating = $('<p>')
                 // Set text to image rating
-                rating.text('Rating: ' + data.rating);
-                // Append gif and raitng to imgDiv
-                imgDiv.append(gif, rating);  
-                // Append imgDiv to gifs element
-                $('#gifs').append(imgDiv);           
-            });
-            // When a image is clicked
-            $("#gifs").on("click", 'img', function() {  
-                // If image is still
-                if($(this).attr('src') == $(this).attr('srcStill')) {
-                    // Change src to the moving image url          
-                    $(this).attr('src', $(this).attr('srcMove'));
-                }
-                // If image is moving
-                else if($(this).attr('src') == $(this).attr('srcMove')) {
-                    // Change src to the still image url          
-                    $(this).attr('src', $(this).attr('srcStill'));
-                }
-            })
-        
-        });
+                .text('Rating: ' + data.rating);
+            // Append gif and raitng to imgDiv
+            imgDiv.append(gif, rating);  
+            // Append imgDiv to gifs element
+            $('#gifs').append(imgDiv);           
+        }); 
+       
     
     });
     
-});
+}
+     
+// When a topic button is clicked
+$(document).on("click", ".topic", callAPI);
+ // When a image is clicked
+ $(document).on("click", '.img', function() {  
 
+    console.log(title);
+    // If image is still
+    if($(this).attr('src') == $(this).attr('srcStill')) {
+        // Change src to the moving image url          
+        $(this).attr('src', $(this).attr('srcMove'));
+    }
+    // If image is moving
+    else if($(this).attr('src') == $(this).attr('srcMove')) {
+        // Change src to the still image url          
+        $(this).attr('src', $(this).attr('srcStill'));
+    }
+})
+
+// renderButtons();
